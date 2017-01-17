044d9e5ffd20bceafafff88a4c89d513cfc15537
Bertie Spell <berniesnell@gmail.com>
2017-01-17 14:02:29 +0000

    added a footer + interactive JS element + external links

--- main.js
+++ main.js
@@ -1,1 +1,24 @@
-console.log("Hello World");
+console.log("hello world");
+$(document).ready(function() {
+    $("").fadeIn(1000);
+});
+
+$(document).ready(function() {
+    $('').mouseenter(function() {
+        $('').hide();
+    });
+});
+
+$(document).ready(function () {
+    $("").fadeTo('fast', 1);
+    });
+
+
+    $(document).ready(function () {
+        $('header nav').mouseenter(function() {
+            $("header nav").fadeTo('fast', 1);
+        });
+        $('header nav').mouseleave(function() {
+            $("header nav").fadeTo('fast', 0.5)
+          });
+        });
