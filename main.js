022b59cd60dba5851d7b214d4cab194202134c77
Bertie Spell <berniesnell@gmail.com>
2017-01-20 10:05:44 +0000

    link working, feature added

--- main.js
+++ main.js
@@ -1,33 +1,8 @@
-console.log("hello world");
-$(document).ready(function() {
-    $("").fadeIn(1000);
-});
-
-$(document).ready(function() {
-    $('').mouseenter(function() {
-        $('').hide();
-    });
-});
-
-$(document).ready(function () {
-    $("").fadeTo('fast', 1);
-    });
-
-
     $(document).ready(function () {
         $('header nav').mouseenter(function() {
             $("header nav").fadeTo('fast', 1);
         });
         $('header nav').mouseleave(function() {
-            $("header nav").fadeTo('fast', 0.5)
+            $("header nav").fadeTo('fast', 0.4)
           });
         });
-
-        $(document).ready(function () {
-            $(".jumbotron").mouseenter(function() {
-                $(".jumbotron").fadeTo(); //add random picture background effect
-            });
-            $(".jumbotron").mouseleave(function() {
-                $(".jumbotron").fadeTo()
-              });
-            });
